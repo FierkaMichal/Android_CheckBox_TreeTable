e739f0116ee4485f2fc7a02048925d46534878ab
Nyrod <pawfr87@gmail.com>
2017-01-14 22:50:54 +0100

    Zrobilem tristate checkox'a

--- app/src/main/java/com/example/hubertfabisiak/checkbox_treetable/MainActivity.java
+++ app/src/main/java/com/example/hubertfabisiak/checkbox_treetable/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.hubertfabisiak.checkbox_treetable;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TristateCheckBox checkBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        RelativeLayout relativeLayout = new RelativeLayout(this);
+        checkBox = new TristateCheckBox(this);
+        relativeLayout.addView(checkBox);
+        relativeLayout.setBackgroundColor(Color.BLACK);
+        setContentView(relativeLayout);
     }
 }
